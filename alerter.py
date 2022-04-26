bf53b2b61bfb1bb05efb0abfea960e1524f676ce
tejeshsai <48875590+tejeshsai@users.noreply.github.com>
2022-04-27 00:32:35 +0530

    Updated Test Cases

--- alerter.py
+++ alerter.py
@@ -1,11 +1,16 @@
 alert_failure_count = 0
 
+
 def network_alert_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
+    if celcius > 175:
+        return 200
+    else:
+        return 500
+
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
@@ -21,5 +26,6 @@ def alert_in_celcius(farenheit):
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
+assert(alert_failure_count == 1)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
